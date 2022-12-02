e60bfe24d400d20efd11e73fa2f627b77f62e8f1
Adam O <66372881+bestadamdagoat@users.noreply.github.com>
2022-12-02 00:02:26 -0600

    Initial Commit

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from bs4 import BeautifulSoup as Soup
 from urllib.request import urlopen as ureq
-import time
 
 queryfile = open('query.txt')
 
 while True:
     query = queryfile.readline()
-    if not query:
-        quit(print("end of file"))
     page = Soup(ureq('https://www.myplates.com/api/licenseplates/passenger/classic-black-silver/' + query).read(), "html.parser").get_text()
-    if "incapsula" in page:
-        quit(print("incapsula"))
-    else:
-        if '"available' in page:
-            print(query)
-    time.sleep(1)
+    if '"available' in page:
+        print(query)
