a61e60f8fbc810b9084f1cd8e542f9487867b0b8
Sourcery AI <>
2023-01-24 17:08:26 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ while True:
     query = queryfile.readline()
     if not query:
         quit(print("end of file"))
-    page = Soup(ureq('https://www.myplates.com/api/licenseplates/passenger/classic-black-silver/' + query).read(), "html.parser").get_text()
+    page = Soup(
+        ureq(
+            f'https://www.myplates.com/api/licenseplates/passenger/classic-black-silver/{query}'
+        ).read(),
+        "html.parser",
+    ).get_text()
     if "incapsula" in page:
         quit(print("incapsula"))
-    else:
-        if '"available' in page:
-            print(query)
+    elif '"available' in page:
+        print(query)
     time.sleep(1)
