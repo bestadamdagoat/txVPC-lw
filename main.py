576a8dd444fbb81443c2fe974dab6044ea90ab6e
Adam O <66372881+bestadamdagoat@users.noreply.github.com>
2023-09-13 09:27:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from bs4 import BeautifulSoup as Soup
 from urllib.request import urlopen as ureq
 
 queryfile = open('query.txt')
 
 while True:
     query = queryfile.readline()
-    page = Soup(ureq('https://www.myplates.com/api/licenseplates/passenger/classic-black-silver/' + query).read(), "html.parser").get_text()
+    page = ureq('https://www.myplates.com/api/licenseplates/passenger/classic-black-silver/' + query).read()
     if '"available' in page:
         print(query)
